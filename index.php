3ec5644aaa3b1274a1c143f1a5e0bd3bddb8a7da
IITS\se19iam <s196282@stud.uni-ruse.bg>
2022-10-25 13:08:47 +0300

    Hyperlink

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        Здравей :)
+	<a href="H:/Cloud_Computing/azure_php/contacts.php"> Kontakti </
+    </body>
+</html>
